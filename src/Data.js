b18c8d967d268d301c8fb6f01a1fbf0671182a8e
Yash Mundankar <90042941+YashMundankar@users.noreply.github.com>
2022-06-26 12:07:16 +0530

    First Commit
    Added Map and Manual JSON Data.

---
+++ src/Data.js
@@ -0,0 +1,118 @@
+export const data=[
+    {
+    
+        "id": 1,
+        "region": "US",
+        "data": 290,
+        "lati": 37.09024,
+        "longi":-95.712891
+    },
+     {
+        "id": 2,
+        "region": "AT",
+        "data": 150,
+        "lati": 47.516231,
+        "longi":14.550072
+     },
+     {
+        "id": 3,
+        "region": "SE",
+        "data": 200,
+        "lati": 60.128161,
+        "longi":18.643501
+     },
+     {
+        "id": 4,
+        "region": "IN",
+        "data": 250,
+        "lati": 20.593684,
+        "longi":78.96288
+     },
+     {
+        "id": 5,
+        "region": "IQ",
+        "data": 150,
+        "lati": 33.223191,
+        "longi":43.679291
+     },
+     {
+        "id": 6,
+        "region": "JP",
+        "data": 150,
+        "lati": 36.204824	,
+        "longi":138.252924
+     },
+     {
+        "id": 7,
+        "region": "KE",
+        "data": 180,
+        "lati": -0.023559	,
+        "longi":37.906193
+     },
+    
+     {
+        "id": 8,
+        "region": "NL",
+        "data": 180,
+        "lati": 52.132633	,
+        "longi":5.291266
+     },
+     {
+        "id": 9,
+        "region": "RU",
+        "data": 280,
+        "lati": 61.52401	,
+        "longi":105.318756	
+     },
+     {
+        "id": 10,
+        "region": "CA",
+        "data": 120,
+        "lati": 56.130366	,
+        "longi":-106.346771	
+     },
+     {
+        "id": 11,
+        "region": "MX",
+        "data": 140,
+        "lati": 23.634501,
+        "longi":-102.552784	
+     },
+     {
+        "id": 12,
+        "region": "BR",
+        "data": 210,
+        "lati": -14.235004	,
+        "longi":-51.92528	
+     },
+     {
+        "id": 13,
+        "region": "AR",
+        "data": 110,
+        "lati": -38.416097	,
+        "longi":-63.616672	
+     },
+     {
+        "id": 14,
+        "region": "CO",
+        "data": 120,
+        "lati": 	4.570868	,
+        "longi":-74.297333
+     },
+     {
+        "id": 15,
+        "region": "ZA",
+        "data": 120,
+        "lati": -30.559482,
+        "longi":22.937506
+     },
+     {
+        "id": 16,
+        "region": "NE",
+        "data": 120,
+        "lati":17.607789,
+        "longi":	8.081666
+     },
+    
+    
+    ]
